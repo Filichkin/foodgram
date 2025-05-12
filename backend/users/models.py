a4070d0abe25a938c3cd3da633879a547f3b7f04
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-05-12 20:00:56 +0400

    added Follow model

--- backend/users/models.py
+++ backend/users/models.py
@@ -60,3 +60,36 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='subscriptions',
+        verbose_name='Follower'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='followers',
+        verbose_name='Author'
+    )
+
+    class Meta:
+        ordering = ('-id',)
+        verbose_name = 'Subscription'
+        verbose_name_plural = 'Subscriptions'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'author'),
+                name='unique_follow'
+            ),
+            models.CheckConstraint(
+                check=~models.Q(author=models.F('user')),
+                name='check_not_self_follow',
+            ),
+        )
+
+    def __str__(self):
+        return f'Subscribe: {self.user.username} for {self.author.username}'

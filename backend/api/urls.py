33d10730f73b33258aa73822773973a322e14b8d
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-05-13 15:21:54 +0400

    created all views and serializers

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from api.views import CustomUserViewSet
+from api.views import (
+    CustomUserViewSet,
+    IngredientViewSet,
+    RecipeViewSet,
+    TagViewSet
+)
 
 
 app_name = 'api'
@@ -9,6 +14,9 @@ app_name = 'api'
 router = DefaultRouter()
 
 router.register('users', CustomUserViewSet, 'users')
+router.register('ingredients', IngredientViewSet, 'ingredients')
+router.register('recipes', RecipeViewSet, 'recipes')
+router.register('tags', TagViewSet, 'tags')
 
 urlpatterns = [
     path('', include(router.urls)),

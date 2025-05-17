67cc299060f69e293e0b2af40feb69f50c12e4f7
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-05-18 00:39:34 +0400

    upd settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -129,7 +129,7 @@ DJOSER = {
         'user_list': ['rest_framework.permissions.AllowAny'],
     },
     'SERIALIZERS': {
-        'user': 'api.serializers.CustomUserSerializer',
-        'current_user': 'api.serializers.CustomUserSerializer',
+        'user': 'api.serializers.UserSerializer',
+        'current_user': 'api.serializers.UserSerializer',
     }
 }

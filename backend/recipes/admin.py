382662f51d814f49633fc878d448bf2229e09660
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-05-17 22:15:06 +0400

    model = Recipe.ingredients_list

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -5,7 +5,7 @@ from recipes.models import Ingredient, Recipe, Tag
 
 
 class RecipeIngredientsInLine(admin.TabularInline):
-    model = Recipe.ingredients.through
+    model = Recipe.ingredients_list.through
     extra = INLINE_EXTRA
 
 

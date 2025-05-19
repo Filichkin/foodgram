23f93438354428daefb0d91075ef18baf57d1850
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-05-19 11:23:27 +0300

    upd IngredientFilter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,10 +1,9 @@
 from django_filters.rest_framework import FilterSet, filters
-from rest_framework.filters import SearchFilter
 
 from recipes.models import Ingredient, Recipe, Tag
 
 
-class IngredientFilter(SearchFilter):
+class IngredientFilter(filters.SearchFilter):
     search_param = 'name'
 
     class Meta:

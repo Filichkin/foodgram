e4e794cb79afa5a536dde426bc99c5695ce86fc6
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-05-19 12:17:40 +0300

    upd IngredientFilter

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,25 +1,23 @@
-import django_filters
-from rest_framework import filters
+from django_filters import rest_framework
+from rest_framework.filters import SearchFilter
 
 from recipes.models import Recipe, Tag
 
 
-class IngredientFilter(filters.SearchFilter):
+class IngredientFilter(SearchFilter):
     search_param = 'name'
 
 
-class RecipeFilter(django_filters.rest_framework.FilterSet):
-    tags = django_filters.rest_framework.filters.ModelMultipleChoiceFilter(
+class RecipeFilter(rest_framework.FilterSet):
+    tags = rest_framework.filters.ModelMultipleChoiceFilter(
+        queryset=Tag.objects.all(),
         field_name='tags__slug',
         to_field_name='slug',
-        queryset=Tag.objects.all(),
-        label='Tags'
     )
-    is_favorited = django_filters.rest_framework.filters.BooleanFilter(
-        method='filter_is_favorited'
+    is_favorited = rest_framework.BooleanFilter(method='filter_is_favorited')
+    is_in_shopping_cart = rest_framework.BooleanFilter(
+        method='filter_is_in_shopping_cart'
     )
-    is_in_shopping_cart = django_filters.rest_framework.filters.BooleanFilter(
-        method='filter_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
